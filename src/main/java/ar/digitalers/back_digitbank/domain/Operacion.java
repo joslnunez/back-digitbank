1e60d2888f388b4f24bb6535d73becf5b7d029d7
LucasFerreyraB <lucas.ferreyrabernasconi@gmail.com>
2023-09-25 21:37:40 -0300

    Commit 4 Clase Cliente agregada + correciones

--- src/main/java/ar/digitalers/back_digitbank/domain/Operacion.java
+++ src/main/java/ar/digitalers/back_digitbank/domain/Operacion.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import domain.Cuenta;
+
 import lombok.Getter;
 import lombok.Setter;
 
